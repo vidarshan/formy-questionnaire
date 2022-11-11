0e16e20d03fb7184a0ab62f4b244e27842db9cb2
Vidarshan <vidarshanadithya3@gmail.com>
2022-11-11 21:40:10 +0530

    added typescript

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Container, Text } from "@mantine/core";
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Container>Default container</Container>
+      <Text size="xl">This is a sample question, answer it?</Text>
+      <Container>Default container</Container>
+    </div>
+  );
+}
+
+export default App;
