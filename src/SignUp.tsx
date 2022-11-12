849cc41fc920b97621ce70776c96f016dddf6cb0
Vidarshan <vidarshanadithya3@gmail.com>
2022-11-12 11:34:44 +0530

    [added] redux toolkit and hooks

---
+++ src/SignUp.tsx
@@ -0,0 +1,12 @@
+import { Flex, Text } from "@mantine/core";
+import React from "react";
+
+const SignUp = () => {
+  return (
+    <Flex>
+      <Text>Sign Up</Text>
+    </Flex>
+  );
+};
+
+export default SignUp;
--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,8 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Conclusion from "./Conclusion";
+import SignUp from "./SignUp";
+import Login from "./Login";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
@@ -13,6 +15,8 @@ root.render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/conclusion" element={<Conclusion />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
