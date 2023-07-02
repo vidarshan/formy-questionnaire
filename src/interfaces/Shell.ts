799be76adfe834f20a250eaec30f18584795c502
Vidarshan <vidarshanadithya3@gmail.com>
2023-07-02 20:48:37 +0530

    Added basic dashboard content

---
+++ src/interfaces/Shell.ts
@@ -0,0 +1,3 @@
+export interface IShell {
+  children: JSX.Element;
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,12 +4,16 @@ import {
   Avatar,
   Box,
   Button,
+  Card,
   Flex,
+  Grid,
   Header,
   Navbar,
   Paper,
+  Table,
   Text,
   ThemeIcon,
+  Title,
 } from "@mantine/core";
 import React from "react";
 import {
@@ -20,93 +24,56 @@ import {
   BsFillHouseFill,
 } from "react-icons/bs";
 import { IoLogOut } from "react-icons/io5";
+import Shell from "../components/Shell";
 
 const Home = () => {
+  const elements = [
+    { position: 6, mass: 12.011, symbol: "C", name: "Carbon" },
+    { position: 7, mass: 14.007, symbol: "N", name: "Nitrogen" },
+    { position: 39, mass: 88.906, symbol: "Y", name: "Yttrium" },
+    { position: 56, mass: 137.33, symbol: "Ba", name: "Barium" },
+    { position: 58, mass: 140.12, symbol: "Ce", name: "Cerium" },
+  ];
+
+  const rows = elements.map((element) => (
+    <tr key={element.name}>
+      <td>{element.position}</td>
+      <td>{element.name}</td>
+      <td>{element.symbol}</td>
+      <td>{element.mass}</td>
+    </tr>
+  ));
+
   return (
-    <AppShell
-      padding="md"
-      navbar={
-        <Navbar width={{ base: 300 }} height="100vh" pl={10} pr={10}>
-          <Flex
-            sx={{ height: "calc(100% - 60px)" }}
-            direction="column"
-            justify="space-between"
-          >
-            <Paper sx={{ backgroundColor: "transparent", width: "100%" }}>
-              <Button
-                mt={10}
-                leftIcon={<BsPlusLg />}
-                radius="xs"
-                color="deep.0"
-                fullWidth
-              >
-                Create New
-              </Button>
-              <Flex mt={20} align="center">
-                <ThemeIcon color="green" radius="xs" variant="light">
-                  <BsFillHouseFill />
-                </ThemeIcon>
-                <Text ml={6} size={14} weight={600}>
-                  Home
-                </Text>
-              </Flex>
-              <Flex mt={20} align="center">
-                <ThemeIcon color="indigo" radius="xs" variant="light">
-                  <BsFileTextFill />
-                </ThemeIcon>
-                <Text ml={6} size={14} weight={600}>
-                  Questionnaires
-                </Text>
-              </Flex>
-              <Flex mt={20} align="center">
-                <ThemeIcon color="red" radius="xs" variant="light">
-                  <BsFillFilePersonFill />
-                </ThemeIcon>
-                <Text ml={6} size={14} weight={600}>
-                  Profile
-                </Text>
-              </Flex>
-            </Paper>
-            <Flex mt={20} mb={20} align="center" justify="space-between">
-              <Flex>
-                <Avatar></Avatar>
-                <Flex direction="column">
-                  <Text ml={6} size={14} weight={300}>
-                    Vidarshan R.
-                  </Text>
-                  <Text ml={6} size={12} weight={600}>
-                    User
-                  </Text>
-                </Flex>
-              </Flex>
-              <ActionIcon color="red" radius="xs" variant="light">
-                <IoLogOut />
-              </ActionIcon>
-            </Flex>
-          </Flex>
-        </Navbar>
-      }
-      header={
-        <Header
-          sx={{ display: "flex", alignItems: "center" }}
-          height={60}
-          p="xs"
-        >
-          <BsReverseListColumnsReverse />
-          <Text sx={{ marginLeft: "10px" }}>Questionnaires</Text>
-        </Header>
-      }
-      styles={(theme) => ({
-        main: {
-          backgroundColor:
-            theme.colorScheme === "dark"
-              ? theme.colors.dark[8]
-              : theme.colors.gray[0],
-        },
-      })}
-    >
-      <Box></Box>
-    </AppShell>
+    <Shell>
+      <>
+        <Text>Dashboard</Text>
+        <Grid>
+          <Grid.Col span={4}>
+            <Card radius="xs" withBorder>
+              <Title>All</Title>
+              <Title>ddd</Title>
+            </Card>
+          </Grid.Col>
+          <Grid.Col span={4}>
+            <Card radius="xs" withBorder>
+              <Title>Opened</Title>
+              <Title>ddd</Title>
+            </Card>
+          </Grid.Col>
+          <Grid.Col span={4}>
+            <Card radius="xs" withBorder>
+              <Title>Closed</Title>
+              <Title>ddd</Title>
+            </Card>
+          </Grid.Col>
+        </Grid>
+        <Text>Questionnaire</Text>
+        <Table striped highlightOnHover>
+          {rows}
+        </Table>
+      </>
+    </Shell>
   );
 };
 
